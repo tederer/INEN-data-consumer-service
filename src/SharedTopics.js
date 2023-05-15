4b66a6802174428e9922be2a2d790064e90e9f58
thomas <thomas@ids.mshome.net>
2023-05-15 19:37:24 +0200

    added example message

--- src/SharedTopics.js
+++ src/SharedTopics.js
@@ -10,6 +10,19 @@ assertNamespace('temperatureui.shared.topics');
  * The server publishes on this topic the current values of the sensors.
  *
  * example: 
- * TODO
+ * [
+ *     {
+ *         sensorId: 'sensor1',
+ *         timestamp: 1684169382069,
+ *         temperature: 4.5,
+ *         geolocation: { longitude: 16.82379644619579, latitude: 47.95404620544281 }
+ *     },
+ *     {
+ *         sensorId: 'sensor2',
+ *         timestamp: 1684169396468,
+ *         temperature: 3.7,
+ *         geolocation: { longitude: 16.82279896886495, latitude: 47.952296393788174 }
+ *     }
+ * ]
  */
 temperatureui.shared.topics.SENSOR_VALUES = '/shared/sensorValues';
